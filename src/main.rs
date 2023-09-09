0b4db662a23af04d59ab473f76582fed32ecd8de
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-09 21:39:51 +0530

    reformat according to rust formatter.

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,44 @@ impl Rectangle {
         self.width * self.height
     }
     fn can_hold(&self, rect: &Rectangle) -> bool {
-        self.area() >= rect.area() && (
-            (self.width >= rect.width && self.height >= rect.height) ||
-                (self.width >= rect.height && self.height >= rect.width)
-        )
+        self.area() >= rect.area()
+            && ((self.width >= rect.width && self.height >= rect.height)
+                || (self.width >= rect.height && self.height >= rect.width))
     }
-    fn make_square(size: u32) -> Rectangle{
-        Rectangle{width: size, height: size}
+    fn make_square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 fn main() {
     let width1 = 30;
     let height1 = 50;
-    let rect1 = Rectangle{ width: width1,  height: height1, };
+    let rect1 = Rectangle {
+        width: width1,
+        height: height1,
+    };
     println!("{:#?}", rect1);
-    println!("The area of the rectangle is {} square pixels.", rect1.area());
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     println!("Can rect1 hold rect1? {}", rect1.can_hold(&rect1));
     let square = Rectangle::make_square(25);
-    println!("Hey this is a square {:#?} with area of {}", square, square.area())
+    println!(
+        "Hey this is a square {:#?} with area of {}",
+        square,
+        square.area()
+    )
 }
