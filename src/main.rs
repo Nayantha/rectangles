e4d84c40c520902b002ba063d2569d3eeea4df2a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-09 17:56:13 +0530

    add logic to print struct using println.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -6,6 +7,7 @@ fn main() {
     let width1 = 30;
     let height1 = 50;
     let rect1 = Rectangle{ width: width1,  height: height1, };
+    println!("{:?}", rect1);
     println!("The area of the rectangle is {} square pixels.", area(&rect1));
 }
 
