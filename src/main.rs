71c3f98c33b104c3d7c1d355836328540cccfb73
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-09 19:48:51 +0530

    add another line to find the rectangle can hold it.

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,5 @@ fn main() {
     let rect3 = Rectangle { width: 60, height: 45 };
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold rect1? {}", rect1.can_hold(&rect1));
 }
