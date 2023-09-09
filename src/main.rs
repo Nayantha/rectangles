825583457c375e866afc11ad48e8ca939ffb3e15
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-09 17:44:45 +0530

    use the new function in main.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let width1 = 30;
     let height1 = 50;
     let rect1 = (width1, height1);
-    println!("The area of the rectangle is {} square pixels.", area(width1, height1));
+    println!("The area of the rectangle is {} square pixels.", dimensions_to_area(rect1));
 }
 
 fn area(width: u32, height: u32) -> u32{
