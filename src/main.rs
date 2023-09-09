6d4c88b40390408cf6e3af813950524c10e674d7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-09 18:00:47 +0530

    implement area calc method as a part of Rectangle struct.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32{
+        rectangle.width * rectangle.height
+    }
+}
 fn main() {
     let width1 = 30;
     let height1 = 50;
     let rect1 = Rectangle{ width: width1,  height: height1, };
     println!("{:#?}", rect1);
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32{
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 }
