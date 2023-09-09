ca069b323625f105cb238e2be605c5055cd56999
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-09 17:52:23 +0530

    add a area calculation function spun from struct.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,10 @@ struct Rectangle {
 fn main() {
     let width1 = 30;
     let height1 = 50;
-    let rect1 = (width1, height1);
-    println!("The area of the rectangle is {} square pixels.", dimensions_to_area(rect1));
+    let rect1 = Rectangle{ width: width1,  height: height1, };
+    println!("The area of the rectangle is {} square pixels.", area(&rect1));
 }
 
-fn area(width: u32, height: u32) -> u32{
-    width * height
-}
-fn dimensions_to_area(dimensions: (u32, u32)) -> u32{
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
 }
